5c472c6ff530c5bb1b74bb60cc4f08634a9730b5
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2023-09-28 11:35:04 +0200

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import Meteo from "./meteo/meteo";
+
+function App() {
+  return (
+    <div className="App">
+      <Meteo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/meteo/meteo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./meteo.styled";
+
+const Meteo: React.FC = () => {
+  return <S.Container>test</S.Container>;
+};
+
+export default Meteo;
